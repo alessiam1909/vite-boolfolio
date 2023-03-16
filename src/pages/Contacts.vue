6dca95c06c0f790588705086f7bb2d781467b1bd
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-16 15:22:05 +0100

    primo abbozzo pagina dei contatti

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+            name: 'Contacts',
+            data(){
+                return{
+
+                }
+            }
+    }
+</script>
+<template lang="">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12">
+                <h1>Pagina contatti: </h1>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12 col-md-6"></div>
+                    <div class="col-12 col-md-6"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
